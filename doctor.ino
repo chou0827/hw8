8340009856c8c8a17b5f5a610febc8f585e54115
chou0827 <35863674+chou0827@users.noreply.github.com>
2018-03-25 20:01:51 -0700

    Create doctor.ino

---
+++ doctor.ino
@@ -0,0 +1,26 @@
+int ledPin1 = 13; 
+
+void setup() {
+ 
+  pinMode(ledPin1, OUTPUT);
+ 
+}
+void loop() {
+
+digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1000);                      
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(100);                       
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(50);                      
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(50);                  
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(50);                      
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(50);        
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1000);                      
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);    
+}
